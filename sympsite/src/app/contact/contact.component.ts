8dd8e2eb299cb6b8d7fee6402706f9a81db2f558
Sanmith Mallikarjun <sanmithmallikarjun@Sanmiths-MacBook-Air.local>
2021-04-17 00:44:46 +0530

    test

--- sympsite/src/app/contact/contact.component.ts
+++ sympsite/src/app/contact/contact.component.ts
@@ -38,7 +38,7 @@ export class ContactComponent implements OnInit {
     this.dataset.description = this.form.value.description
     console.log(this.dataset.description);
     
-    this.https.post<Details>('https://162.241.74.209:8081/getdetails', this.dataset).subscribe(
+    this.https.post<Details>('https://127.0.0.1:8081/getdetails', this.dataset).subscribe(
       res => {
         this.dataset = res;
         console.log(this.dataset);
